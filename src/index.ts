ede47598ccc983ec6be9de67538bf235d2b0ec44
AMGavrilov <amgavri5@mts.ru>
2020-11-30 22:47:21 +0300

    add date choose

--- src/index.ts
+++ src/index.ts
@@ -22,22 +22,36 @@ import { TARIFF } from "./constants/tariff";
     await page.goto(bookingSetting.url)
 
     //выбор волны
-    await page.waitForSelector('.BookingOptions-tariff')
+    const TARIFF_CLASS = '.BookingOptions-tariff'
+    await page.waitForSelector(TARIFF_CLASS)
     await page.evaluate((bookingSetting) => {
-        const tariffList = document.querySelectorAll('.BookingOptions-tariff .BookingOptions-item')
+        const tariffList = document.querySelectorAll(`${TARIFF_CLASS} .BookingOptions-item`)
         const tariff: HTMLElement = tariffList[bookingSetting.tariff.order] as HTMLElement
         tariff.click()
     }, bookingSetting);
 
 
     //выбор количество персон
-    await page.waitForSelector('.BookingDetails')
+    const BOOKING_DETAILS_CLASS = '.BookingDetails'
+    await page.waitForSelector(BOOKING_DETAILS_CLASS)
     await page.evaluate((bookingSetting) => {
-        const personNumbers = document.querySelectorAll(`.BookingDetails button`)
+        const personNumbers = document.querySelectorAll(`${BOOKING_DETAILS_CLASS} button`)
         console.log(personNumbers)
         const personNumber: HTMLElement = personNumbers[bookingSetting.person - 1] as HTMLElement
         console.log(personNumber)
         personNumber.click()
+    }, bookingSetting);
+
+    //выбор даты и времени
+    const TIME_TABLE_CLASS = '.wave_booking_timetable'
+    await page.waitForSelector(TIME_TABLE_CLASS)
+    await page.evaluate((bookingSetting) => {
+        const tableBody: NodeListOf<Element> = document.querySelectorAll(`${TIME_TABLE_CLASS} tbody`)
+        const timeCellIndex = [...tableBody].querySelectorAll('th')
+        // findIndex(cell => cell.textContent === bookingSetting.date.time)
+        console.log(timeCellIndex)
+        // const personNumber: HTMLElement = personNumbers[bookingSetting.person - 1] as HTMLElement
+        // personNumber.click()
     }, bookingSetting);
 
     // await browser.close();
